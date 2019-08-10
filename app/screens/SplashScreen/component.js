d60f5c26f1398164dd62b095f5f301cf8257ac60
Rizky Febrianto Dwiputra <rizkyfebriantodwiputra@gmail.com>
2019-08-10 16:42:33 +0700

    Enable SplashScreen Functional

--- app/screens/SplashScreen/component.js
+++ app/screens/SplashScreen/component.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 import IMAGES from '../../configs/images';
 import I18n from '../../i18n';
 
 export default class Component extends React.Component {
+  async componentDidMount() {
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.props.navigation.navigate('Home');
+    }
+  }
+
+  performTimeConsumingTask = async () =>
+    new Promise(resolve =>
+      setTimeout(() => {
+        resolve('result');
+      }, 2000)
+    );
+
   render() {
     return (
       <View style={styles.mainContainer}>
+        <StatusBar hidden />
         <View style={styles.logoContainer}>
           <Image source={IMAGES.appLogo} resizeMode="contain" style={styles.logo} />
         </View>
@@ -18,3 +35,7 @@ export default class Component extends React.Component {
     );
   }
 }
+
+Component.propTypes = {
+  navigation: PropTypes.object.isRequired
+};
